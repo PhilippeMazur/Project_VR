f95053b236c7ddf0f1bbeebf6995d7568f40614d
RensIgnoul <ignoulrens0@gmail.com>
2023-06-08 19:54:07 +0200

    initial commit

--- Assets/Scripts/SeekerAgent.cs
+++ Assets/Scripts/SeekerAgent.cs
@@ -11,11 +11,14 @@ public class SeekerAgent : Agent // mlagents-learn config/SeekerAgent.yaml --run
     private DateTime _startTime;
     private int _lastIndex;
     private Rigidbody _rigidbody;
+    public float rayLength;
+    private bool _hasFoundHider;
     public override void OnEpisodeBegin() 
     {
         _rigidbody ??= GetComponentInChildren<Rigidbody>();
+        _rigidbody.velocity = Vector3.zero;
         _hiders ??= GameObject.FindGameObjectsWithTag("Hider").ToArray();
-        transform.localPosition = new Vector3(-37.9f, 0, 0.2f);
+        transform.localPosition = new Vector3(-40f, 0, 2.4f);
         _hiders.ToList().ForEach(h => h.SetActive(false));
         int currentIndex = _lastIndex;
         while (currentIndex == _lastIndex)
@@ -27,6 +30,8 @@ public class SeekerAgent : Agent // mlagents-learn config/SeekerAgent.yaml --run
         _hiders[currentIndex-1].SetActive(true);
 
         _startTime = DateTime.Now;
+
+        _hasFoundHider = false;
     }
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -34,55 +39,94 @@ public class SeekerAgent : Agent // mlagents-learn config/SeekerAgent.yaml --run
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        AddReward(-0.0002f);
-        var fwd = actions.ContinuousActions[0]; //go forward and backwards
-        var rotation = actions.ContinuousActions[1]; //turn left or right
+        AddReward(-0.0001f);
+        MoveAgent(actions.DiscreteActions);
+        /*   AddReward(-0.0002f);
+           var fwd = actions.ContinuousActions[0];
+           var strafe = actions.ContinuousActions[1];//go forward and backwards
+           var rotation = actions.ContinuousActions[2]; //turn left or right
 
-        //make agent move forward or backward
-        //transform.forward = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + fwd * 10f * Time.deltaTime);
-        transform.localPosition +=  Math.Max(fwd,0) * 8f * Time.deltaTime*transform.forward;
-        //make agent rotate
-        transform.Rotate(transform.up * rotation * 10f);
-        
 
-        var ray = new Ray(this.transform.position, this.transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit,70f))
-        {
-            if (hit.transform.gameObject.CompareTag("Hider"))
-            {                
-                var duration = DateTime.Now - _startTime;
-                Debug.Log($"found object in: {duration.Minutes} minutes and {duration.Seconds} seconds");
-                AddReward(1);
-               // AddReward(1f - (float)(duration.TotalSeconds/TimeSpan.FromMinutes(2).TotalSeconds));
-                EndEpisode();
-            }
-        }
-        //if((DateTime.Now - _startTime).TotalMinutes >= 1)
-        //{
-         //   AddReward(-0.5f);
-         //   Debug.Log("Times up!");
-         //   transform.position = new Vector3(0, 0, 0);
-        //    EndEpisode();
-       // }
+           // Make agent move forward or backward
+           transform.localPosition += transform.forward * fwd * 8f * Time.deltaTime;
+
+           // Make agent strafe left or right
+           transform.localPosition += transform.right * strafe * 8f * Time.deltaTime;
+
+           // Make agent rotate
+           var rotationAngle = rotation * 10f;
+           transform.Rotate(transform.up, rotationAngle);
+
+           ///
+           //make agent move forward or backward
+           //transform.forward = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + fwd * 10f * Time.deltaTime);
+         //  transform.localPosition +=  Math.Max(fwd,0) * 8f * Time.deltaTime*transform.forward;
+           //make agent rotate*/
+
+        //var ray = new Ray(this.transform.position, this.transform.forward);
+        //if (Physics.Raycast(ray, out RaycastHit hit,rayLength))
+       // {
+         //   if (hit.transform.gameObject.CompareTag("Hider") && !_hasFoundHider)
+          //  {                
+           //     var duration = DateTime.Now - _startTime;
+            //    Debug.Log($"found object in: {duration.Minutes} minutes and {duration.Seconds} seconds");
+               // AddReward(0.25f);
+             //   _hasFoundHider = true;
+            //}
+        //}
     }
     public override void Heuristic(in ActionBuffers actionsOut)
-    {        
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Vertical");
-        continuousActionsOut[1] = Input.GetAxis("Horizontal");
-        
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+
+    }
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 200f);
+        _rigidbody.AddForce(dirToGo, ForceMode.VelocityChange);
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Hider"))
         {
-            AddReward(1f);
+            AddReward(2f);
             EndEpisode();
-        }
-        else if (collision.gameObject.CompareTag("Obstacle"))
-        {
-          //  AddReward(-1.0f);
-          //  EndEpisode();
         }
     }
 }
+
